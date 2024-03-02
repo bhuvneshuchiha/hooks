4bf0fb912fc8f4ebf4f7818dae04851047b60a91
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-03-03 02:46:53 +0530

    hooks_prac

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  
+  useEffect(() => {
+   axios.get("https://sum-server.100xdevs.com/todos")
+   .then((res) => {
+    setTodos(res.data.todos)
+   })
+  }, []);
 
   return  <div>
     {todos.map((todo) => {
